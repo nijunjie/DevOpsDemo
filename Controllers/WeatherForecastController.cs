7c1cee0dd1b5537e47738f4ce78596ca8161444b
Jason Ni (PACTERA TECHNOLOGIES INC) <v-jani@microsoft.com>
2020-12-21 16:43:57 +0800

    Updated WeatherForecastController.cs file

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace weatherapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
